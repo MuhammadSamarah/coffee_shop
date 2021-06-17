194979f1bdfff4be1c6a963884704f9c66414c02
MuhammadSamarah <muhammad.samarah@bryanuniversity.edu>
2021-06-17 10:57:09 -0500

    Updated script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const form = document.getElementById("items");
+const b = 3;
+const z = 7;
+const x = 11;
 
 function it(num1, num2) {
     return (num1 * num2);
@@ -7,9 +10,9 @@ function it(num1, num2) {
 function totalCost(e){
     e.preventDefault();
  
-    let numberOfbagels = it(form.bagels.value, 3);
-    let numberOftea = it(form.tea.value, 7);
-    let numberOfcoffee = it(form.coffee.value, 11);
+    let numberOfbagels = it(form.bagels.value, b);
+    let numberOftea = it(form.tea.value, z);
+    let numberOfcoffee = it(form.coffee.value, x);
 
     let result = (numberOfbagels + numberOftea + numberOfcoffee);
 
